c28550a53aee542ab45e18cddaaa7e766a192ed1
bastiaba <bastian.ballmann@inf.ethz.ch>
2017-11-21 14:02:21 +0100

    added GITLAB_DIR config

--- gitlab_config.py
+++ gitlab_config.py
@@ -1,6 +1,7 @@
 TOKEN="tokenofadminuser"
 CLONE_ACCESS_TOKEN="token_to_clone_via_https"
 SERVER="gitlab.your-domain.tld"
+GITLAB_DIR="/opt/gitlab/"
 REPOSITORY_DIR="/var/opt/gitlab/git-data/repositories"
 BACKUP_DIR="/path/to/your/backups"
 UPLOAD_DIR="/var/opt/gitlab/gitlab-rails/uploads"
